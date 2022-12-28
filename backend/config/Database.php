d8a02017b560ac11188576e31a95faa8da632402
Yusuf <mdyusuf2521@gmail.com>
2022-12-28 20:26:25 +0530

    please ignore

--- backend/config/Database.php
+++ backend/config/Database.php
@@ -1,12 +1,11 @@
 <?php
     class Database{
-        private $server = "localhost";
-        private $username = "yusuf";
+        private $server="localhost";
+        private $username="yusuf";
         private $password = "yusuf";
-
-        public function getConnection(){
-            $connection = new mysqli($this->server, $this->username, $this->password);
-            return $connection;
+        function getConnection(){
+              $connection = new mysqli($this->server, $this->username, $this->password);
+              return $connection;
         }
     }
 ?>
