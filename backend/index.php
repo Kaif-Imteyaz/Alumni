e45fd388e71217bcab5054d5bf9af378ce5abdc2
Yusuf <mdyusuf2521@gmail.com>
2023-01-14 19:38:25 +0530

    extended ajax functionality for login

--- backend/index.php
+++ backend/index.php
@@ -11,6 +11,7 @@
         header('Access-Control-Allow-Origin:http://127.0.0.1:5500');
         header('Access-Control-Allow-Headers:content-type');
         header('Access-Control-Allow-Method:POST,GET,OPTIONS');
+
         exit;
     }   
     
--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
 
         document.getElementById('signup').style.visibility = 'visible';
         document.getElementById('flip-card-btn-turn-to-back').style.visibility = 'visible';
@@ -17,41 +17,94 @@ document.addEventListener('DOMContentLoaded', function(event) {
         document.getElementById('flip-card').classList.toggle('do-flip');
         };
 
+        
+        function sendRequest(url,method,queryString,data,callback){
 
-        let signupForm=document.querySelector('#register');
-
-        signupForm.addEventListener('submit',function(e){
-              e.preventDefault();
-              let data={};
-              for(let i=0;i<this.elements.length;i++){
-                if(this.elements[i].type!='submit'){
-                  if(this.elements[i].type=='date'){
-                    console.log(this.elements[i].value);
-                  }
-                  data[this.elements[i].name]=this.elements[i].value;
-                }
+            data= typeof(data)=='object' && data!=null ? data : {};
+            queryString=typeof(data)=='object' && queryString!=null ?queryString:{};
+            
+            //adding the querystring if available to the url
+            if(Object.keys(queryString)>0){
+              url+='?';
+              for(let key in queryString){
+                url+=key+"="+queryString[key];
+                url+="&";
               }
+            }
 
-              console.log(this.method);
-              let jsonData=JSON.stringify(data);
+            let jsonData=JSON.stringify(data);
+            
+            let xhr=new XMLHttpRequest();
+            xhr.open(method,url);
+            
+            xhr.addEventListener("load",()=>{
+              callback(xhr.status,xhr.response);
+            })
+            
+            xhr.addEventListener('error',(err)=>{
+              console.log(err);
+            })
+            
+            xhr.send(jsonData);
+        }
 
-              //sending the data to the backend using XMLHttpRequest
-              let xhr=new XMLHttpRequest();
-              xhr.open(this.method,this.action);
+        function formResponseHandler(formId,requestPayload,responsePayload){
+            if(formId=='signup'){
+              //login the user after they have successfully been registered.
+              let data={
+                "type":requestPayload.type,
+                "email":requestPayload.email,
+                "password":requestPayload.password,
+              }
+              sendRequest("http://localhost:8000/createSession","POST",undefined,data,(status,response)=>{
+                  if(status!=200 && status!=201){
+                      document.querySelector("#"+formId+" .formError").textContent=response;
+                  }
+                  else{
+                    //we will receive the token
+                  }
+              })
+            }
 
-              xhr.addEventListener("load",()=>{
-                if(xhr.readyState!=200 && xhr.readyState!=201){
-                  let formError=document.querySelector('#register .formError');
-                  console.log(formError);
-                  formError.textContent=xhr.responseText.toUpperCase();
+            if(formId=="login"){
+                //we have received the token
+                console.log(responsePayload);
+            } 
+        }
+        
+        //-----------------------------
+
+        let forms=document.querySelectorAll('form');
+        forms.forEach(form=>{
+          form.addEventListener('submit',function(e){
+            e.preventDefault();
+            let data={};
+            let method=this.method.toUpperCase();
+            let url=this.action;
+            let queryString={};
+
+            for(let i=0;i<this.elements.length;i++){
+              if(this.elements[i].type!='submit'){
+                data[this.elements[i].name]=this.elements[i].value;
+              }
+            }
+            
+            //sending the data to the backend 
+           sendRequest(url,method,queryString,data,(statusCode,response)=>{
+              //all bad requests
+                if(statusCode!=200 && statusCode!=201){
+                    let error=typeof(response)=='string' && response.length<60?response:"an error has occured";
+                    console.log(error);
+                    document.querySelector('#'+this.id+" .formError").textContent=error.toUpperCase();
                 }
-              })
+                else{
+                  formResponseHandler(this.id,data,response);   
+                }
+           });
 
-              xhr.addEventListener('error',(err)=>{
-                console.log(err);
-              })
+      })
+    })
 
-              xhr.send(jsonData);
-        })
 
-});
+
+})
